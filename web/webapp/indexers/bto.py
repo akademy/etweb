a51c6b3a880ee742a55d734de2cfbd38c08a96d2
J Matthew B Wilcoxson <matthew.wilcoxson@private.email>
2023-10-02 21:18:01 +0100

    Index bto data!

---
+++ web/webapp/indexers/bto.py
@@ -0,0 +1,88 @@
+import csv
+import os
+from datetime import datetime, timedelta
+
+from webapp.models import Analysis, Detection, Detector, Position, Species
+from webapp.indexers.indexer import Indexer
+
+
+class BTO(Indexer):
+
+	CONFIDENCE_LIMIT = 0.5
+	
+	def index_data(self, position, detector, analysis, file_path_list):
+		
+		for path in file_path_list:
+
+
+			species_dict = {}
+			with open(path, newline='') as csv_file:
+				reader = csv.DictReader(csv_file)
+				for row in reader:
+
+					if row["SPECIES"] in ["bird", "No ID"] :
+						continue
+
+					confidence = float(row["PROBABILITY"])
+					if confidence < BTO.CONFIDENCE_LIMIT :
+						continue
+					
+					
+					detection_time = datetime.strptime(f"{row['ACTUAL DATE']} {row['TIME']}", "%d/%m/%Y %H:%M:%S")
+					scientific_name = row['SCIENTIFIC NAME']
+					
+					if scientific_name not in species_dict:
+						try:
+							species = Species.objects.get(scientific_name__iexact=scientific_name)
+						except Species.DoesNotExist:
+							species = Species.objects.create(
+								common_name=row['ENGLISH NAME'],
+								scientific_name=scientific_name,
+								description=""
+							)  # TODO: Add "Species Group" and "Species" to Species table
+						species_dict[scientific_name] = species
+
+
+					try:
+						d = Detection.objects.get(
+							date__exact=detection_time,
+							species=species_dict[scientific_name],
+							detector=detector,
+							analysis=analysis
+						)
+						if d.confidence < confidence:
+							d.confidence = confidence
+							d.save()
+						
+					except Detection.DoesNotExist:
+						d = Detection.objects.create(
+							date=detection_time,
+							species=species_dict[scientific_name],
+							detector=detector,
+							analysis=analysis,
+							confidence=confidence
+						)
+
+		return True
+
+	
+	@staticmethod
+	def _datetime_from_filename(path):
+		"""
+		Extract date and time from filename
+			e.g. "EARTH_20220809_025600.BirdNET.results.csv"
+		:param filename: filename or filepath
+		:return: dict {'filename': filename, 'date': date, 'time': time}
+		:raise: ValueError if can't read data from name
+		"""
+		filename = os.path.basename(path).replace(",", "-")
+		date_and_time = filename.split("_")
+		date = date_and_time[1]
+		time = date_and_time[2].split(".")[0]
+
+		# test are numbers
+		_ = int(date)
+		_ = int(time)
+
+		return datetime.strptime(f"{date} {time}", "%Y%m%d %H%M%S")
+		
