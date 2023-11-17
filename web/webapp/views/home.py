e8d7c496a5a8b4c0dbade070d8a231389c932987
J Matthew B Wilcoxson <matthew.wilcoxson@private.email>
2023-11-17 17:51:10 +0000

    Added stats display. Choose icons for datasets.

--- web/webapp/views/home.py
+++ web/webapp/views/home.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
 
-from ..models import Species
+
+from ..models import Analysis, Detection, Detector, Position, Species
 
 
 def index(request):
-	return render(request, "home.html")
+
+	position_number = Position.objects.count()
+	detection_number = Detection.objects.count()
+	species_number = Species.objects.count()
+	
+	context = {
+		"position_number": position_number,
+		"detection_number": detection_number,
+		"species_number": species_number,
+	}
+	
+	return render(request, "home.html", context )
 
 
 def explore(request):
-	return render(request, "explore.html")
+
+	analysis_number = Analysis.objects.count()
+	detection_number = Detection.objects.count()
+	detector_number = Detector.objects.count()
+	position_number = Position.objects.count()
+	species_number = Species.objects.count()
+
+	context = {
+		"analysis_number": analysis_number,
+		"detection_number": detection_number,
+		"detector_number": detector_number,
+		"position_number": position_number,
+		"species_number": species_number,
+	}
+	
+	return render(request, "explore.html", context )
 
